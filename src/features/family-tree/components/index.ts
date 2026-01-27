04e3f5044bb31ccdccbeea43367e7a63c97d7ce8
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2026-01-28 01:15:23 +0800

    Add barrel file for family tree components
    Created an index.ts file to re-export FamilyTree, PersonNode, AddPersonDialog, and PersonDetailPanel components for easier imports.

---
+++ src/features/family-tree/components/index.ts
@@ -0,0 +1,4 @@
+export { FamilyTree } from './family-tree';
+export { PersonNode } from './person-node';
+export { AddPersonDialog } from './add-person-dialog';
+export { PersonDetailPanel } from './person-detail-panel';
