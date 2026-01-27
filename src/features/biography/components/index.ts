b6377317891b68db0bb34a49b18bea2671c19887
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2026-01-28 02:14:53 +0800

    Create index.ts

---
+++ src/features/biography/components/index.ts
@@ -0,0 +1,4 @@
+export { BiographyView } from './biography-view';
+export { BiographySidebar } from './biography-sidebar';
+export { BiographyContent } from './biography-content';
+export { BiographyEntry } from './biography-entry';
