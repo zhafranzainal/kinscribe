9b4bfd553d3e9f05e5ea879056b66dd7d33dc550
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2026-01-28 09:41:42 +0800

    Create index.ts

---
+++ src/features/wedding/components/index.ts
@@ -0,0 +1,4 @@
+export { WeddingView } from './wedding-view';
+export { GuestListPanel } from './guest-list-panel';
+export { AddGuestDialog } from './add-guest-dialog';
+export { CreateWeddingDialog } from './create-wedding-dialog';
